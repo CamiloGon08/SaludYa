68fc1f22d5843c5e000e599c3f424d93af474d06
Camilo Gonzalez <cagonzalezgarcia@unicesar.edu.co>
2026-06-26 15:53:33 -0500

    feat:Estructura inicial Spring Boot - SaludYa

---
+++ src/main/java/com/example/saludya/saludya/SaludyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.saludya.saludya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saludya/saludya/SaludyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saludya.saludya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
